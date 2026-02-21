4f76a65e1e8edaa9378f2b4ab36463c94a504f17
DragLed <155687013+DragLed@users.noreply.github.com>
2026-02-21 13:02:41 +0300

    new api > https://api.dragledwl.ru

--- src/shared/api/baseRequest.ts
+++ src/shared/api/baseRequest.ts
@@ -2,11 +2,11 @@ import { Users } from './generated/Users'
 import { Gifts } from './generated/Gifts'
 
 export const GiftsRequest = new Gifts({
-  baseURL: 'https://127.0.0.1:8000',
+  baseURL: 'https://api.dragledwl.ru',
   withCredentials: true,
 })
 
 export const UserRequest = new Users({
-  baseURL: 'https://127.0.0.1:8000',
+  baseURL: 'https://api.dragledwl.ru',
   withCredentials: true,
 })
